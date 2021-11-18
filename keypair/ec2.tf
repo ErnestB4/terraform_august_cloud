69b131ee8a2b6abe39d205e5a78ea460560d7a58
ErnestB4 <88800168+ErnestB4@users.noreply.github.com>
2021-11-17 19:54:40 -0600

    added

--- keypair/ec2.tf
+++ keypair/ec2.tf
@@ -22,11 +22,19 @@ resource "aws_instance" "web" {
     Name = "HelloWorld"
   }
 
-resource "aws_instance" "web1" {
+resource "aws_instance" "web2" {
   ami           = data.aws_ami.ubuntu.id
   instance_type = "t3.micro"
 
   tags = {
-    Name = "HelloWorld"
+    Name = "HelloWorld2"
+  }
+}
+resource "aws_instance" "web3" {
+  ami           = data.aws_ami.ubuntu.id
+  instance_type = "t3.micro"
+
+  tags = {
+    Name = "HelloWorld3"
   }
 }
